6fe0dd06ffdcfe8d8b6e028f754c311c43a058db
Silas Souza <146384122+Silas26@users.noreply.github.com>
2024-09-16 19:49:40 -0300

    Update app.js
    adicionei o addEventListener 'keypress' que realiza a pesquisa ao pressionar a tecla entender

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 
+const campoPesquisa = document.getElementById("campo-pesquisa");
+const sectionResultados = document.getElementById("resultados-pesquisa");
+
+// Adiciona o event listener para o evento 'keypress'
+campoPesquisa.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault(); // Impede o comportamento padrão (quebrar a linha)
+        pesquisar(); // Chama a função de pesquisa
+    }
+});
 function pesquisar() {
-    
     let section = document.getElementById
     ("resultados-pesquisa");
 
@@ -16,7 +25,6 @@ function pesquisar() {
     let resultados = "";
     let nome = "";
     let posicao = "";
-
     for (let dado of jogadores) {
       nome = dado.nome.toLowerCase()
       posicao = dado.posicao.toLowerCase() 
@@ -29,6 +37,7 @@ function pesquisar() {
                 <a href="" target="_blank">${dado.nome}</a>
                 </h2>
                 <p class="descriçao-meta">
+                <img src='${dado.image}'>
                 <p><strong>Posição: </strong>${dado.posicao}</p>
                 <p><strong>Data de Nascimento: </strong>${dado.dataNascimento}</p>
                 <p><strong>Altura: </strong>${dado.altura}</p>
@@ -37,16 +46,19 @@ function pesquisar() {
                 <p><strong>Idade: </strong>${dado.idade}</p>
                 <p><strong>Clube: </strong>${dado.clube}</p>
                 <p><strong>Valor de Mercado: </strong>${dado.valorMercado}</p>
+                
                 <p>
                     <a href="${dado.link}" target="_blank">Mais Informações</a>
                 </p>
                 </div>
     `
-}
-if (!resultados) {
-  resultados = "<p>Esse resultado não existe</p>"
+  }
+  
+  if (!resultados) {
+  
+    resultados = "<p>Esse resultado não existe</p>"
 
-}
+}   
 
 section.innerHTML = resultados
     }
